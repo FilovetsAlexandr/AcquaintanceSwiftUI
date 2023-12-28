208b261cf6ea208d6e8b9723c53de20f236bf4cb
FilovetsAlexandr <sohan.boxan@gmail.com>
2023-12-28 17:40:02 +0300

    project created

---
+++ AlertSwiftUI/AlertSwiftUI/AlertSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlertSwiftUIApp.swift
+//  AlertSwiftUI
+//
+//  Created by Alexandr Filovets on 28.12.23.
+//
+
+import SwiftUI
+
+@main
+struct AlertSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
